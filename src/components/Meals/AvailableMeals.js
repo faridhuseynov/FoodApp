da2c17cdc2d5921101ea2617c04b5056ef513043
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-05-03 21:52:48 +0400

    meal data properly fetched from database

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,51 +1,68 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Card from "../UI/Card/Card"
 
 import classes from "./AvailableMeals.module.css"
 import MealItem from "./MealItem/MealItem";
 
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+// let DUMMY_MEALS = [
+//     {
+//         id: 'm1',
+//         name: 'Sushi',
+//         description: 'Finest fish and veggies',
+//         price: 22.99,
+//     },
+//     {
+//         id: 'm2',
+//         name: 'Schnitzel',
+//         description: 'A german specialty!',
+//         price: 16.5,
+//     },
+//     {
+//         id: 'm3',
+//         name: 'Barbecue Burger',
+//         description: 'American, raw, meaty',
+//         price: 12.99,
+//     },
+//     {
+//         id: 'm4',
+//         name: 'Green Bowl',
+//         description: 'Healthy...and green...',
+//         price: 18.99,
+//     },
+// ];
 
 const AvailableMeals = () => {
+    const [meals,setMeals]=useState([]);
+    useEffect(()=>{
+        const fetchMeals=async()=>{
+            try {
+                const response = await fetch("https://foodapp-8cd60-default-rtdb.firebaseio.com/meals.json");
+                const responseData = await response.json();
 
-    useEffect( async()=>{
-        try {
-            const response = await fetch({url:"https://foodapp-8cd60-default-rtdb.firebaseio.com/tasks.json",method:'GET'});
-            console.log(response);
-            
-        } catch (error) {
-            console.log(error.message);
+                const loadedMeals=[];
+
+                for(const key in responseData){
+                    
+                    loadedMeals.push({
+                        id:key,
+                        name:responseData[key].name,
+                        description:responseData[key].description,
+                        price:responseData[key].price
+                    });
+                }
+                setMeals(loadedMeals);
+            } catch (error) {
+                console.log(error);
+            }
         }
-    },[])
 
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} id={meal.id}
-        description={meal.description} price={meal.price} />)
+        fetchMeals();
+    },[]);
+
+
+     const mealsList = meals.map(meal => <MealItem key={meal.id} name={meal.name}
+     id={meal.id} description={meal.description} price={meal.price} />)
     return <section className={classes.meals}>
         <Card>
             <ul>
